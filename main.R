879d27ad42ddb532598a2400d4f88dfd29df9dcc
ginberg <info@gerinberg.com>
2021-05-21 10:12:33 +0000

    rename protocol id

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(ijtiff)
 
 TAG_LIST  <- list("date_time" = "DateTime", "barcode" = "Barcode", "col" = "Col", "cycle" = "Cycle", "exposure time" = "Exposure Time", "filter" = "Filter", 
-                  "ps12" = "PS12", "row" = "Row", "temperature" = "Temperature", "timestamp" = "Timestamp", "instrument unit" = "Instrument Unit", "protocol id" = "Protocol ID")
+                  "ps12" = "PS12", "row" = "Row", "temperature" = "Temperature", "timestamp" = "Timestamp", "instrument unit" = "Instrument Unit", "run id" = "RunId")
 TAG_NAMES <- as.vector(unlist(TAG_LIST))
 IMAGE_COL <- "Image"
 
